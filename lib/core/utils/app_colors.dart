96ccb80940bce23327a7ec4d937ea4faf356ee89
OmarKhaled00e0 <ok4966541@gmail.com>
2025-07-02 03:17:25 +0300

    add Styles App

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = Color(0xFF617AFD);
+  static Color secodaryColor = Color(0xFF8391A1);
+  static Color blackColor = Color(0xFF1F2C37);
+  static Color greyColor = Color(0xFF9CA4AB);
+  static Color whiteColor = Colors.white;
+}
